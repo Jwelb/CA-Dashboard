83cdee0d8edd08b18c36918d1baf1959edf18057
GrantMelvin <Grantmelvin4@gmail.com>
2023-09-26 17:06:05 -0400

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,55 @@
 import React from 'react'
-import {Stack, IconButton} from "@chakra-ui/react"
+import {Stack, IconButton, useDisclosure, Collapse, Box, List, ListItem} from "@chakra-ui/react"
 import { HamburgerIcon } from '@chakra-ui/icons'
 
 import NavbarData from './NavbarData'
 import { useNavigate } from "react-router" ;
-import { useState } from 'react';
 
 
 function Navbar() {
 
   const navigate = useNavigate()
 
-  const [isOpen, setIsOpen] = useState(true)
+  const { isOpen, onToggle } = useDisclosure()
 
   return (
-    <Stack className="Sidebar">
+    <Stack
+    className="Sidebar">
       <IconButton
-      height='4vh'
-      onClick={() => {
-        setIsOpen(!isOpen)
-      }}
-      id='contextMenu'
       icon={<HamburgerIcon/>}
+      height='4vh'
+      onClick={onToggle}
+      id='contextMenu'
+      mb='-2'
+      bg='RGBA(255, 255, 255, 0.08)'
       />
-      <ul className='sideBarList' id={isOpen === true ? "show" : "hidden"}>
-      <li className="row" id="name"></li>
-        <li className="row"></li>
-        {NavbarData.map((val, key) => {
-        return (
-          <li key={key} 
-            className="row"
-            id="link"
-            onClick={() => {
-              navigate(val.link)
-            }}>
-            <div id="title">{val.title}</div>
-          </li>
-          )})}
-      </ul>
-
+      <Collapse in={!isOpen} animateOpacity>
+        <Stack 
+        className="Sidebar"
+        bg={isOpen? '' : '#171923'}
+        rounded={'lg'}
+        >
+          
+          <List className='sideBarList'>
+            {NavbarData.map((val, key) => {
+            return (
+              <ListItem 
+                key={key} 
+                className="row"
+                mt={2}
+                mb={2}
+                id={window.location.pathname === val.link ? "active" : "" }
+                rounded={'lg'}
+                onClick={() => {
+                  navigate(val.link)
+                }}>
+                <Box id="icon" mr={3} >{val.icon}</Box>
+                <Box id="title">{val.title}</Box>
+              </ListItem>
+              )})}
+          </List>
+        </Stack>
+      </Collapse>
     </Stack>
   )
 }
