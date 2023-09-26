03e72be5069d513ca1c94f9fa307c55981f5b4fc
GrantMelvin <Grantmelvin4@gmail.com>
2023-09-26 18:17:14 -0400

    Search Page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import { Field, Formik, Form } from 'formik'
 import * as Yup from "yup";
-import { HStack, VStack, Input, Text, Box, Button, Card, CardHeader, Heading, Stack, StackDivider, CardBody, Tabs, Tab, TabList, Center, IconButton, Flex } from "@chakra-ui/react";
+import { HStack, VStack, Container, Text, Box, Button, Card, CardHeader, Heading, Stack, StackDivider, CardBody, Tabs, Tab, TabList, Center, IconButton, Flex } from "@chakra-ui/react";
 import { SearchIcon } from '@chakra-ui/icons'
 import { useDisclosure } from "@chakra-ui/react";
 import { useState } from 'react';
@@ -20,24 +20,16 @@ function Search() {
 
   const [answer, setAnswer] = useState('initial')
   const [resource, setResource] = useState('Internal')
+  const resources = ['Internal', 'Google', 'Wikipedia']
+
   const { isOpen, onOpen, onClose } = useDisclosure()
   const cancelRef = React.useRef()
+
   const getResponse = ({ ...props }) => {
     setAnswer(props.question)
     console.log(props.question)
     // Need to connect to back-end
   }
-  /*initialValues={{ question: '' }}
-        validationSchema={Yup.object({
-          question: Yup.string().required("Please enter a question")
-        })}
-        onSubmit={(values, actions) => {
-          const vals = { ...values };
-          //console.log(vals)
-          getResponse(vals)
-          actions.resetForm()
-        }}
-  */
   return (
     <Formik
       initialValues={{ question: '' }}
@@ -56,13 +48,12 @@ function Search() {
       {(formik) => (
         <HStack
           w="100%"
-
           as={Form}>
-
 
           <VStack>
             {Navbar()}
           </VStack>
+
           {/* Alert Box */}
           <AlertDialog
             isOpen={isOpen}
@@ -80,103 +71,96 @@ function Search() {
 
                 <AlertDialogFooter>
                   <Button ref={cancelRef} onClick={onClose}>
-                    Roger that, batman.
+                    Roger that.
                   </Button>
                 </AlertDialogFooter>
               </AlertDialogContent>
             </AlertDialogOverlay>
-
-
           </AlertDialog>
 
-          <Flex alignItems='center' justifyContent="center" mb='93vh'>
+          <VStack height='100vh' w='100vw'>
 
-            <TextField
-              name='question'
-              placeholder={"Search Query goes here"}
-              autoComplete="off"
-              width='70vw'
-              height='5vh'
-              borderColor="black"
-            />
-            <IconButton type='submit' aria-label='Search database' icon={<SearchIcon />} />
-          </Flex>
-          <Flex h='50vh' w='50vw' bg='grey' alignItems='center' justifyContent='center'>
-            <Card opacity={answer === 'initial' ? 0 : 1}>
-              <HStack padding={1}>
-                <Tabs>
-                  <TabList>
-                    <Tab id='link'
-                      onClick={() => {
-                        setResource('Internal')
-                      }}>
-                      <Text size='sm'>Internal Resource</Text>
-                    </Tab>
-                    <Tab id='link'
-                      onClick={() => {
-                        setResource('Google')
-                      }}>
-                      <Text size='sm'>Google Resource</Text>
-                    </Tab>
-                    <Tab id='link'
-                      onClick={() => {
-                        setResource('Wikipedia')
-                      }}>
-                      <Text size='sm'>Wikipedia Resource</Text>
-                    </Tab>
-                  </TabList>
-                </Tabs>
-              </HStack>
+            <HStack mt='5vh'>
+              <TextField
+                name='question'
+                placeholder={"Search Query goes here"}
+                autoComplete="off"
+                width='70vw'
+                height='5vh'
+                borderColor="black"
+              />
+              <IconButton type='submit' aria-label='Search database' icon={<SearchIcon />} />
+            </HStack>
 
-              <Card>
-                <Stack divider={<StackDivider />} spacing='.3'>
-                  <HStack>
-                    <CardBody>
-                      <Text size='xs' fontWeight='bold'>
-                        {answer} article 1 from {resource}
-                      </Text>
-                      <Text pt='1' fontSize='sm'>
-                        ...
-                      </Text>
-                    </CardBody>
-                  </HStack>
-                  <HStack>
-                    <CardBody>
-                      <Text size='xs' fontWeight='bold'>
-                        {answer} article 1 from {resource}
-                      </Text>
-                      <Text pt='1' fontSize='sm'>
-                        ...
-                      </Text>
-                    </CardBody>
-                  </HStack>
-                  <HStack>
-                    <CardBody>
-                      <Text size='xs' fontWeight='bold'>
-                        {answer} article 1 from {resource}
-                      </Text>
-                      <Text pt='1' fontSize='sm'>
-                        ...
-                      </Text>
-                    </CardBody>
-                  </HStack>
-                  <HStack>
-                    <CardBody>
-                      <Text size='xs' fontWeight='bold'>
-                        {answer} article 1 from {resource}
-                      </Text>
-                      <Text pt='1' fontSize='sm'>
-                        ...
-                      </Text>
-                    </CardBody>
-                  </HStack>
-                </Stack>
+            <Box mt='5vh' w='80vw'>
+              <Card opacity={answer === 'initial' ? 0 : 1}>
+                <HStack padding={1}>
+                  <Tabs alignItems={'center'}>
+                    <TabList alignItems={'center'}>
+                      {resources.map((item, index) => {
+                        return(
+                          <Tab 
+                          id='link'
+                          key={index}
+                          onClick={() => {
+                            setResource(item)
+                          }}>
+                            <Text size='sm'>{item}</Text>
+                          </Tab>
+                        )
+                      })}
+                    </TabList>
+                  </Tabs>
+                </HStack>
+
+                <Card>
+                  <Stack divider={<StackDivider />} spacing='.3'>
+                    <HStack>
+                      <CardBody>
+                        <Text size='xs' fontWeight='bold'>
+                          {answer} article 1 from {resource}
+                        </Text>
+                        <Text pt='1' fontSize='sm'>
+                          ...
+                        </Text>
+                      </CardBody>
+                    </HStack>
+                    <HStack>
+                      <CardBody>
+                        <Text size='xs' fontWeight='bold'>
+                          {answer} article 1 from {resource}
+                        </Text>
+                        <Text pt='1' fontSize='sm'>
+                          ...
+                        </Text>
+                      </CardBody>
+                    </HStack>
+                    <HStack>
+                      <CardBody>
+                        <Text size='xs' fontWeight='bold'>
+                          {answer} article 1 from {resource}
+                        </Text>
+                        <Text pt='1' fontSize='sm'>
+                          ...
+                        </Text>
+                      </CardBody>
+                    </HStack>
+                    <HStack>
+                      <CardBody>
+                        <Text size='xs' fontWeight='bold'>
+                          {answer} article 1 from {resource}
+                        </Text>
+                        <Text pt='1' fontSize='sm'>
+                          ...
+                        </Text>
+                      </CardBody>
+                    </HStack>
+                  </Stack>
+                </Card>
               </Card>
-            </Card>
-          </Flex>
+            </Box>
 
-
-
+          </VStack>
         </HStack>
       )}
     </Formik>
